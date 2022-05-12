db926c39a0c3226423eb668f469c2094e2c5f5ce
Itamar Stern <itamar.stern@imperva.com>
2022-05-12 10:52:50 +0300

    second commit

--- server2.js
+++ server2.js
@@ -4,15 +4,24 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
+  const { rawHeaders, httpVersion, method, socket, url } = req;
+  const { remoteAddress, remoteFamily } = socket;
+  console.log(
+    JSON.stringify({
+      timestamp: Date.now(),
+      rawHeaders,
+      httpVersion,
+      method,
+      remoteAddress,
+      remoteFamily,
+      url
+    })
+  );
   res.end()
-  console.log(req)
 })
 
-app.post('/', (req, res) => {
-  res.end()
-  console.log(req)
-})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`)
